32a45e47f16ae749811890588325550fc7f40467
eithel03 <eithel.herrera03@gmail.com>
2025-12-07 01:35:44 -0600

    Lógica completa de ataques según raza/arma y implementación de imágenes de cada raza junto a la lógica para mostrarlas

---
+++ Proyecto_2_Movil/Converters/RazaToImagenConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace Proyecto_2_Movil.Converters
+{
+    public class RazaToImagenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string raza = value?.ToString() ?? "";
+
+            return raza switch
+            {
+                "Humano" => "humano.jpeg",
+                "Elfo" => "elfo.jpeg",
+                "Elfo (Agua)" => "elfo_agua.jpeg",
+                "Orco" => "orco.jpeg",
+                "Bestia" => "bestia.jpeg",
+                _ => "personaje_default.jpeg"
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
